a43532e6a679773281911013e292327cf32d72c1
liztanyl <elizabeth.tanyulin@gmail.com>
2022-05-25 22:11:29 +0800

    add state to product card & avail colour display - add 'selectedColour' state to product card & available colour display - update colour display to squares

--- src/components/ProductListing/AvailableColourDisplay.js
+++ src/components/ProductListing/AvailableColourDisplay.js
@@ -1,30 +1,66 @@
-import { Checkbox, Tooltip } from '@mui/material';
-import { CircleTwoTone, CircleRounded } from '@mui/icons-material';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { Box, Typography, Checkbox, Tooltip } from '@mui/material';
+import { SquareRounded } from '@mui/icons-material';
+import { BACKEND_URL } from '../../store.js';
 
-export default function AvailableColourDisplay({ productId, colours }) {
-	// TODO: axios.get all available colours & compare product colours vs available colours
-	// eg if red, black and yellow are available, but this product only has red and black. should show all 3 colours but with yellow disabled
+export default function AvailableColourDisplay({
+	productId,
+	productColours,
+	selectedColour,
+	setSelectedColour,
+}) {
+	// retrieves all available colours & compares product colours vs available colours
+	// eg if red, black and yellow are available, but this product only has red and black, should show all 3 colours but with yellow disabled
+
+	const [availableColours, setAvailableColours] = useState([]);
+
+	useEffect(() => {
+		axios.get(`${BACKEND_URL}/colours`).then((response) => {
+			setAvailableColours(response.data);
+		});
+	}, []);
+
+	const updateSelectedColour = (e) => {
+		const clickedColour = e.target.value;
+		clickedColour === selectedColour
+			? setSelectedColour(null) // unselect colour
+			: setSelectedColour(clickedColour); // set as selected colour
+	};
 
 	function AvailableColours() {
-		return colours.map((colour) => {
-			console.log(colour);
+		return availableColours.map((colour) => {
 			return (
 				<Tooltip title={colour.name} key={`${productId}-${colour.name}`}>
-					<Checkbox
-						value={colour.name}
-						icon={<CircleTwoTone />}
-						checkedIcon={<CircleRounded />}
-						sx={{
-							color: colour.code,
-							'&.Mui-checked': {
+					<span>
+						<Checkbox
+							value={colour.name}
+							checked={colour.name === selectedColour}
+							onChange={updateSelectedColour}
+							icon={<SquareRounded />}
+							disabled={!productColours.includes(colour.id)}
+							sx={{
+								px: 1,
 								color: colour.code,
-							},
-						}}
-					/>
+								'&.Mui-checked': {
+									color: colour.code,
+								},
+							}}
+						/>
+					</span>
 				</Tooltip>
 			);
 		});
 	}
 
-	return <AvailableColours />;
+	return (
+		<Box mt={2}>
+			<Typography variant="body1" color="text.secondary">
+				{selectedColour
+					? `Available colours (selected: ${selectedColour})`
+					: 'Available colours (click to select)'}
+			</Typography>
+			<AvailableColours />
+		</Box>
+	);
 }
--- src/components/ProductListing/ProductSummary.js
+++ src/components/ProductListing/ProductSummary.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
 	Grid,
 	Card,
@@ -11,29 +12,49 @@ import {
 import AvailableColourDisplay from './AvailableColourDisplay.js';
 
 export default function ProductSummary({ productDetails }) {
+	const [selectedColour, setSelectedColour] = useState(null);
+
 	const { id, name, usualPrice, currentPrice, colours } = productDetails;
+	const colourIds = colours.map((colour) => colour.id);
+
+	function UsualPrice() {
+		return (
+			<Typography variant="subtitle1" color="text.secondary">
+				(Usual price: S${usualPrice})
+			</Typography>
+		);
+	}
 
 	return (
 		<Grid item xs={5}>
-			<Card>
+			<Card sx={{ px: 2, pt: 3, pb: 5 }}>
 				{/* <CardMedia component="img" image={image} /> */}
 				<CardContent>
 					<Typography gutterBottom variant="h5">
 						{name}
 					</Typography>
-					<Typography variant="h6" color="text.secondary">
-						S${currentPrice}
-					</Typography>
-					<Typography variant="subtitle1" color="text.secondary">
-						(Usual price: S${usualPrice})
-					</Typography>
-					<AvailableColourDisplay productId={id} colours={colours} />
+					<Typography variant="h6">S${currentPrice}</Typography>
+					{usualPrice !== currentPrice && <UsualPrice />}
+					<AvailableColourDisplay
+						productId={id}
+						productColours={colourIds}
+						selectedColour={selectedColour}
+						setSelectedColour={setSelectedColour}
+					/>
 				</CardContent>
-				<CardActions>
-					<Button size="small" href={`/product/${id}`}>
+				<CardActions
+					sx={{
+						flexDirection: 'column',
+						alignItems: 'flex-start',
+						px: 2,
+					}}
+				>
+					<Button size="small" href={`/product/${id}`} sx={{ mb: 2 }}>
 						View details
 					</Button>
-					<Button size="small">Add to Cart</Button>
+					<Button size="small" variant="contained">
+						Add to Cart
+					</Button>
 				</CardActions>
 			</Card>
 		</Grid>
