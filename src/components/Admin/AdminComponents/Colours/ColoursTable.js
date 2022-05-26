a87e7f499447d6ac2dfc73b43ff8d3c42a4f34b1
gcskhor <gcskhor@yahoo.com.sg>
2022-05-26 22:39:09 +0800

    started colours table

---
+++ src/components/Admin/AdminComponents/Colours/ColoursTable.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { IconButton, Box, Checkbox } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+export default function ColoursTable({ colours }) {
+  console.log(colours);
+  return <div>ColoursTable</div>;
+}
