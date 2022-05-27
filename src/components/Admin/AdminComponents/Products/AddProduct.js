4d11276d95eebb6b6623ce775945571e1146edb3
gcskhor <gcskhor@yahoo.com.sg>
2022-05-27 09:19:20 +0800

    added select colour checkbox functionality

--- src/components/Admin/AdminComponents/Products/AddProduct.js
+++ src/components/Admin/AdminComponents/Products/AddProduct.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { BACKEND_URL } from "../../../../store.js";
 import {
@@ -15,20 +15,43 @@ import {
 export default function AddProduct() {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
+  const [features, setFeatures] = useState("");
+  const [outerDimensions, setOuterDimensions] = useState("");
+  const [mounting, setMounting] = useState("");
+  const [materials, setMaterials] = useState("");
   const [usualPrice, setUsualPrice] = useState("");
   const [currentPrice, setCurrentPrice] = useState("");
   const [available, setAvailable] = useState(false);
   const [colours, setColours] = useState([]);
+  const [allColours, setAllColours] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${BACKEND_URL}/admin/colours`)
+      .then((response) => {
+        console.log(response.data);
+        setAllColours(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   const dataToServer = {
     name: name,
     description: description,
+    features: features,
+    outerDimensions: outerDimensions,
+    mounting: mounting,
+    materials: materials,
     usualPrice: usualPrice,
     currentPrice: currentPrice,
     available: available,
+    colours: colours,
   };
 
   const handleAdd = () => {
+    console.log(dataToServer);
     axios
       .post(`${BACKEND_URL}/admin/product/add`, dataToServer)
       .then((response) => {
@@ -37,6 +60,17 @@ export default function AddProduct() {
       .catch((err) => {
         console.log(err);
       });
+  };
+
+  const handleColourOnChange = (event, actionableColour) => {
+    if (event.target.checked) {
+      setColours((colours) => [...colours, actionableColour]);
+    } else {
+      const coloursCopy = [...colours];
+      setColours(
+        coloursCopy.filter((colour) => colour.id !== actionableColour.id)
+      );
+    }
   };
 
   return (
@@ -62,6 +96,54 @@ export default function AddProduct() {
           setDescription(event.target.value);
         }}
         value={description}
+      />
+      <TextField
+        label="Features"
+        placeholder="Features"
+        variant="filled"
+        multiline
+        rows={2}
+        fullWidth
+        onChange={(event) => {
+          setFeatures(event.target.value);
+        }}
+        value={features}
+      />
+      <TextField
+        label="Outer Dimensions"
+        placeholder="Outer Dimensions"
+        variant="filled"
+        multiline
+        rows={1}
+        fullWidth
+        onChange={(event) => {
+          setOuterDimensions(event.target.value);
+        }}
+        value={outerDimensions}
+      />
+      <TextField
+        label="Mounting"
+        placeholder="Mounting"
+        variant="filled"
+        multiline
+        rows={4}
+        fullWidth
+        onChange={(event) => {
+          setMounting(event.target.value);
+        }}
+        value={mounting}
+      />
+      <TextField
+        label="Materials"
+        placeholder="Materials"
+        variant="filled"
+        multiline
+        rows={4}
+        fullWidth
+        onChange={(event) => {
+          setMaterials(event.target.value);
+        }}
+        value={materials}
       />
       <TextField
         label="Usual Price (SGD)"
@@ -96,7 +178,27 @@ export default function AddProduct() {
           label="Available"
         />
       </FormGroup>
-      <Box>Colours</Box>
+      <Box>
+        <Box>Colours</Box>
+        {allColours.map((colour) => {
+          return (
+            <FormGroup key={colour.name}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    label={colour.name}
+                    labelplacement="start"
+                    onChange={(event) => {
+                      handleColourOnChange(event, colour);
+                    }}
+                  />
+                }
+                label={colour.name}
+              />
+            </FormGroup>
+          );
+        })}
+      </Box>
 
       <Button
         fullWidth
