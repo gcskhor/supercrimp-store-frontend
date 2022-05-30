c9db990d830ed88ac6e6a50552ce8f67a6dbc6a5
gcskhor <gcskhor@yahoo.com.sg>
2022-05-30 18:09:23 +0800

    added admin login component and route

---
+++ src/components/Admin/AdminComponents/Login.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { Button, Box, CardMedia, TextField } from "@mui/material";
+import { BACKEND_URL } from "../../../store";
+import axios from "axios";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    axios
+      .post(`${BACKEND_URL}/admin/login`, { email: email, password: password })
+      .then((response) => {
+        console.log(response.data.token);
+
+        // login success
+        response.data.token &&
+          localStorage.setItem("token", response.data.token);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <Box>
+      <Box component="form" sx={{ width: "400px", height: "1000px", m: 5 }}>
+        <CardMedia
+          component="img"
+          image="/images/logos/SUPERCRIMP-logo-black.png"
+          alt=""
+        />
+        <h2>Admin Login</h2>
+        <TextField
+          label="Email Address"
+          placeholder="admin@supercrimp.com"
+          variant="filled"
+          fullWidth
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+          value={email}
+        />
+        <TextField
+          label="Password"
+          type="password"
+          placeholder="*********"
+          variant="filled"
+          fullWidth
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+          value={password}
+        />
+
+        <Button
+          fullWidth
+          variant="contained"
+          // component={Link}
+          // to={`/admin`}
+          onClick={() => {
+            handleLogin();
+          }}
+        >
+          Log In
+        </Button>
+      </Box>
+    </Box>
+  );
+}
