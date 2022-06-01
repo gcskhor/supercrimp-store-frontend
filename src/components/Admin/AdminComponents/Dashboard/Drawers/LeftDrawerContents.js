7a192bcf7b087eb86f5767743030272b8039a765
gcskhor <gcskhor@yahoo.com.sg>
2022-06-01 15:28:04 +0800

    added admin logout button

--- src/components/Admin/AdminComponents/Dashboard/Drawers/LeftDrawerContents.js
+++ src/components/Admin/AdminComponents/Dashboard/Drawers/LeftDrawerContents.js
@@ -1,72 +1,105 @@
 import React from "react";
+import axios from "axios";
 import {
-  Toolbar,
-  Divider,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
+	Toolbar,
+	Divider,
+	List,
+	ListItem,
+	ListItemButton,
+	ListItemIcon,
+	ListItemText,
 } from "@mui/material";
 
 import HomeIcon from "@mui/icons-material/Home";
 import CategoryIcon from "@mui/icons-material/Category";
 import ColorLensIcon from "@mui/icons-material/ColorLens";
+import MeetingRoomIcon from "@mui/icons-material/MeetingRoom";
 
 import { Link } from "react-router-dom";
 
+import { BACKEND_URL } from "../../../../../store.js";
+
 export default function LeftDrawerContents() {
-  return (
-    <div>
-      <Toolbar />
-      <Divider />
-      <List>
-        <ListItem
-          key="home"
-          component={Link}
-          to="/admin"
-          sx={{ color: "black" }}
-          disablePadding
-        >
-          <ListItemButton>
-            <ListItemIcon>
-              <HomeIcon />
-            </ListItemIcon>
-            <ListItemText primary="Home" />
-          </ListItemButton>
-        </ListItem>
+	const handleLogout = () => {
+		axios
+			.post(`${BACKEND_URL}/admin/logout`)
+			.then((response) => {
+				console.log(response);
+				console.log("cleared cookies");
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
 
-        <ListItem
-          key="products"
-          component={Link}
-          to="/admin/products"
-          sx={{ color: "black" }}
-          disablePadding
-        >
-          <ListItemButton>
-            <ListItemIcon>
-              <CategoryIcon />
-            </ListItemIcon>
-            <ListItemText primary="Products" />
-          </ListItemButton>
-        </ListItem>
+	return (
+		<div>
+			<Toolbar />
+			<Divider />
+			<List>
+				<ListItem
+					key="admin-home-leftdrawer"
+					component={Link}
+					to="/admin"
+					sx={{ color: "black" }}
+					disablePadding
+				>
+					<ListItemButton>
+						<ListItemIcon>
+							<HomeIcon />
+						</ListItemIcon>
+						<ListItemText primary="Home" />
+					</ListItemButton>
+				</ListItem>
 
-        <ListItem
-          key="colours"
-          component={Link}
-          to="/admin/colours"
-          sx={{ color: "black" }}
-          disablePadding
-        >
-          <ListItemButton>
-            <ListItemIcon>
-              <ColorLensIcon />
-            </ListItemIcon>
-            <ListItemText primary="Colours" />
-          </ListItemButton>
-        </ListItem>
-      </List>
-      <Divider />
-    </div>
-  );
+				<ListItem
+					key="admin-products-leftdrawer"
+					component={Link}
+					to="/admin/products"
+					sx={{ color: "black" }}
+					disablePadding
+				>
+					<ListItemButton>
+						<ListItemIcon>
+							<CategoryIcon />
+						</ListItemIcon>
+						<ListItemText primary="Products" />
+					</ListItemButton>
+				</ListItem>
+
+				<ListItem
+					key="admin-colours-leftdrawer"
+					component={Link}
+					to="/admin/colours"
+					sx={{ color: "black" }}
+					disablePadding
+				>
+					<ListItemButton>
+						<ListItemIcon>
+							<ColorLensIcon />
+						</ListItemIcon>
+						<ListItemText primary="Colours" />
+					</ListItemButton>
+				</ListItem>
+
+				<Divider />
+
+				<ListItem
+					key="admin-logout-leftdrawer"
+					component={Link}
+					to="/admin/login"
+					sx={{ color: "black" }}
+					disablePadding
+					onClick={handleLogout}
+				>
+					<ListItemButton>
+						<ListItemIcon>
+							<MeetingRoomIcon />
+						</ListItemIcon>
+						<ListItemText primary="Log Out" />
+					</ListItemButton>
+				</ListItem>
+			</List>
+		</div>
+	);
 }
