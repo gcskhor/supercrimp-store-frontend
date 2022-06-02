c4e91da46ed2f88ed38c2b4ff8e252e2f1b18207
gcskhor <gcskhor@yahoo.com.sg>
2022-06-02 10:35:15 +0800

    - Relocated SnackbarContext to App level - fixed snackbar context path in all customer components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import { ThemeContextProvider } from "./components/ThemeContext.js";
+import { SnackbarContextProvider } from "./components/SnackbarContext.js";
+import Snackbars from "./components/Snackbars.js";
 
 import Customer from "./components/Customer/Customer.js";
 import Home from "./components/Customer/Home.js";
@@ -21,14 +23,34 @@ import Login from "./components/Admin/AdminComponents/Login.js";
 export default function App() {
 	return (
 		<ThemeContextProvider>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Customer />}>
-						<Route index element={<Home />} />
-						<Route path="product/:productId" element={<Product />} />
-						<Route path="cart" element={<Cart />} />
-						<Route path="checkout" element={<Checkout />} />
-						<Route path="order/:orderId" element={<OrderSuccess />} />
+			<SnackbarContextProvider>
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<Customer />}>
+							<Route index element={<Home />} />
+							<Route path="product/:productId" element={<Product />} />
+							<Route path="cart" element={<Cart />} />
+							<Route path="checkout" element={<Checkout />} />
+							<Route path="order/:orderId" element={<OrderSuccess />} />
+							<Route
+								path="*"
+								element={
+									<main>
+										<p>There's nothing here!</p>
+									</main>
+								}
+							/>
+						</Route>
+						<Route path="/admin/login" element={<Login />} />
+						<Route path="/admin" element={<Admin />}>
+							<Route path="products" element={<Products />} />
+							<Route path="product">
+								<Route path="add" element={<AddProduct />} />
+								<Route path=":productId/edit" element={<EditProduct />} />
+							</Route>
+							<Route path="colours" element={<Colours />} />
+						</Route>
+						<Route path="colours" element={<Colours />} />
 						<Route
 							path="*"
 							element={
@@ -37,27 +59,10 @@ export default function App() {
 								</main>
 							}
 						/>
-					</Route>
-					<Route path="/admin/login" element={<Login />} />
-					<Route path="/admin" element={<Admin />}>
-						<Route path="products" element={<Products />} />
-						<Route path="product">
-							<Route path="add" element={<AddProduct />} />
-							<Route path=":productId/edit" element={<EditProduct />} />
-						</Route>
-						<Route path="colours" element={<Colours />} />
-					</Route>
-					<Route path="colours" element={<Colours />} />
-					<Route
-						path="*"
-						element={
-							<main>
-								<p>There's nothing here!</p>
-							</main>
-						}
-					/>
-				</Routes>
-			</BrowserRouter>
+					</Routes>
+				</BrowserRouter>
+				<Snackbars />
+			</SnackbarContextProvider>
 		</ThemeContextProvider>
 	);
 }
