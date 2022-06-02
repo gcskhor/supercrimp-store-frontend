d3ca5115c721c0c3d1f6ba1b9ca10a6ced514cd5
gcskhor <gcskhor@yahoo.com.sg>
2022-06-02 13:34:55 +0800

    removed unnecessary code

--- src/components/Admin/AdminComponents/Colours.js
+++ src/components/Admin/AdminComponents/Colours.js
@@ -9,55 +9,52 @@ import AddColourDialog from "./Colours/AddColourDialog.js";
 import DeleteColourDialog from "./Colours/DeleteColourDialog";
 
 export function reloadColours(setColours, BACKEND_URL) {
-  return axios
-    .get(`${BACKEND_URL}/admin/colours`)
-    .then((response) => {
-      setColours(response.data);
-    })
-    .catch((err) => {
-      console.log(err.response);
-      if (err.response.status === 401) {
-        window.location.href = "/admin/login";
-      }
-    });
+	return axios
+		.get(`${BACKEND_URL}/admin/colours`)
+		.then((response) => {
+			setColours(response.data);
+		})
+		.catch((err) => {
+			console.log(err.response);
+		});
 }
 
 export default function Colours() {
-  const [colours, setColours] = useState([]);
-  const [selectedColour, setSelectedColour] = useState(null);
-  const [openAddDialog, setOpenAddDialog] = useState(false);
-  const [openEditDialog, setOpenEditDialog] = useState(false);
-  const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
+	const [colours, setColours] = useState([]);
+	const [selectedColour, setSelectedColour] = useState(null);
+	const [openAddDialog, setOpenAddDialog] = useState(false);
+	const [openEditDialog, setOpenEditDialog] = useState(false);
+	const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
 
-  useEffect(() => {
-    reloadColours(setColours, BACKEND_URL);
-  }, []);
-  return (
-    <Box>
-      <ColourContext.Provider
-        value={{
-          coloursContext: [colours, setColours],
-          selectedColourContext: [selectedColour, setSelectedColour],
-          openAddDialogContext: [openAddDialog, setOpenAddDialog],
-          openEditDialogContext: [openEditDialog, setOpenEditDialog],
-          openDeleteDialogContext: [openDeleteDialog, setOpenDeleteDialog],
-        }}
-      >
-        <h2>Colours</h2>
-        <ColoursTable />
-        <Button
-          fullWidth
-          variant="contained"
-          onClick={() => {
-            setOpenAddDialog(true);
-          }}
-        >
-          Add Colour
-        </Button>
-        {openAddDialog && <AddColourDialog />}
-        {openEditDialog && selectedColour && <EditColourDialog />}
-        {openDeleteDialog && selectedColour && <DeleteColourDialog />}
-      </ColourContext.Provider>
-    </Box>
-  );
+	useEffect(() => {
+		reloadColours(setColours, BACKEND_URL);
+	}, []);
+	return (
+		<Box>
+			<ColourContext.Provider
+				value={{
+					coloursContext: [colours, setColours],
+					selectedColourContext: [selectedColour, setSelectedColour],
+					openAddDialogContext: [openAddDialog, setOpenAddDialog],
+					openEditDialogContext: [openEditDialog, setOpenEditDialog],
+					openDeleteDialogContext: [openDeleteDialog, setOpenDeleteDialog],
+				}}
+			>
+				<h2>Colours</h2>
+				<ColoursTable />
+				<Button
+					fullWidth
+					variant="contained"
+					onClick={() => {
+						setOpenAddDialog(true);
+					}}
+				>
+					Add Colour
+				</Button>
+				{openAddDialog && <AddColourDialog />}
+				{openEditDialog && selectedColour && <EditColourDialog />}
+				{openDeleteDialog && selectedColour && <DeleteColourDialog />}
+			</ColourContext.Provider>
+		</Box>
+	);
 }
