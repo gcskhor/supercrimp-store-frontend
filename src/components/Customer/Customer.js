ff92b78feb3b237bad9f3fb20a818ac7f67dd409
gcskhor <gcskhor@yahoo.com.sg>
2022-06-02 18:11:49 +0800

    updated snackbar context to fix image reload bug

--- src/components/Customer/Customer.js
+++ src/components/Customer/Customer.js
@@ -2,16 +2,19 @@ import { Outlet } from "react-router-dom";
 
 import { InventoryContextProvider } from "./InventoryContext.js";
 import { CartContextProvider } from "./CartContext.js";
+import { SnackbarContextProvider } from "../SnackbarContext.js";
 import Navbar from "./Navbar/Navbar.js";
-// import Snackbars from "./Snackbars.js";
+import Snackbars from "../Snackbars.js";
 
 function Customer() {
 	return (
 		<InventoryContextProvider>
 			<CartContextProvider>
-				<Navbar />
-				<Outlet />
-				{/* <Snackbars /> */}
+				<SnackbarContextProvider>
+					<Navbar />
+					<Outlet />
+					<Snackbars />
+				</SnackbarContextProvider>
 			</CartContextProvider>
 		</InventoryContextProvider>
 	);
