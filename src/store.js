55c03a5f073972bb2c20834e073545a5e449d856
gcskhor <gcskhor@yahoo.com.sg>
2022-07-18 12:59:38 +0800

    added deployment link

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
 export const BACKEND_URL =
   window.location.origin === 'http://localhost:3000'
     ? 'http://localhost:3004'
-    : 'http://3.82.120.69:3004';
+    : 'https://supercrimp.herokuapp.com';
